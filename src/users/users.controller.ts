08233adba80e0f057a646467c6a43676e78d5185
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-12-08 01:56:33 +0200

    Added user guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,11 +4,8 @@ import {
   Put,
   Param,
   Body,
-  HttpCode,
-  HttpStatus,
   UseGuards,
   Request,
-  Post,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,14 +13,6 @@ import { UpdateUserBalanceDto } from './dto/update-user-balance.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
 import { UsersService } from './users.service';
-
-interface IFilteredUser {
-  readonly nickname: string;
-  readonly email: string;
-  readonly avatar: string;
-  readonly dollarBalance: number;
-  readonly lastBonusTime: number;
-}
 
 @Controller('users')
 export class UsersController {
@@ -40,11 +29,13 @@ export class UsersController {
     return user;
   }
   
+  // @UseGuards(JwtAuthGuard)
   @Get('/balance/:id')
   getBalance(@Param('id') id: string): Promise<number> {
     return this.usersService.getBalance(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   updateUser(
     @Param('id') id: string,
@@ -53,6 +44,7 @@ export class UsersController {
     return this.usersService.updateUser({ id, userDto });
   }
 
+  // @UseGuards(JwtAuthGuard)
   @Put('balance/:id')
   updateBalance(
     @Param('id') id: string,
