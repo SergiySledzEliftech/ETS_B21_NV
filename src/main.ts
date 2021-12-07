d3eb858ae065b6fdea04fdf21ba587898bcdbae3
Oleg Shevchenko <ama.freelnc@gmail.com>
2021-12-07 04:18:00 +0200

    Adds swagger
    Добавил сваггер на эндпоинт '/documentation'

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Trainder')
+    .setDescription('Trainder API endpoints description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
